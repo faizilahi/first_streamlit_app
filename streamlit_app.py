a4b1f582ea75b228fba7b9dcc3a1925fe05ee089
faizilahi <faiz.ilahi@gmail.com>
2023-08-13 18:18:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit
 
-# Note: change this value for a different result
-streamlit.num = 8 
-
-# To take the input from the user
-#num = float(input('Enter a number: '))
-
-streamlit.num_sqrt = num ** 0.5
-print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+streamlit.title('badshah ki kahani jo kahin kho gaya')
+streamlit.header('ek baar aisa hua')
+streamlit.text('faiz naami ek naujawan kahin kho gaya')
