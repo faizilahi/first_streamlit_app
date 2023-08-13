aa768d4eac1ea0eb20d92a3d1db85ac13619489c
faizilahi <faiz.ilahi@gmail.com>
2023-08-13 18:15:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit
 
 # Note: change this value for a different result
-num = 8 
+streamlit.num = 8 
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
 
-num_sqrt = num ** 0.5
+streamlit.num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
