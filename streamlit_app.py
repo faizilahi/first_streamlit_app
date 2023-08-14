25203c7782a9f9a925c391b83034fecd1e5a2016
faizilahi <faiz.ilahi@gmail.com>
2023-08-14 10:46:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,27 @@
-import streamlit
+import st as st
 import pandas
 import requests
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.header('Breakfast Favourites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list= my_fruit_list.set_index('Fruit')
 options=list(my_fruit_list.index)
 default=['Avocado','Strawberries']
-fruits_selected=streamlit.multiselect("pick some fruits:",options,default)
+fruits_selected=st.multiselect("pick some fruits:",options,default)
 fruits_to_show=my_fruits_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+st.write(fruits_selected)
+st.write(type(fruits_selected))
+st.write(my_fruit_list.head())
+st.dataframe(fruits_to_show)
 
-# streamlit.header("Fruityvice Fruit Advice!")
+# st.header("Fruityvice Fruit Advice!")
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response)
+# st.text(fruityvice_response)
